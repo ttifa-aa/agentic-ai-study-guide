f503adab267f08ba76a944e4ff2c187a6c57518f
ttifa-aa <aatifatami@gmail.com>
2026-03-23 21:22:30 +0530

    removed unnecessary files

--- temp.py
+++
@@ -1,8 +0,0 @@
-old_prompt = '''
-You are an academic assistant. Answer the question using ONLY the context below.
-If the answer isn't in the context, say "I couldn't find that in the uploaded document."
-context: {context}
-
-                                          
-question: {question}
-answer:'''
